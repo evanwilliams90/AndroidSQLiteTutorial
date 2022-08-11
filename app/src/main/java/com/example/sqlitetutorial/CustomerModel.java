b4b590b4840861e8523f0036ceeca177a46b3557
evanwilliams90 <erwilliams@hotmail.co.nz>
2022-08-11 21:05:06 +1200

    up to end of part 5: create DB and insert records

---
+++ app/src/main/java/com/example/sqlitetutorial/CustomerModel.java
@@ -0,0 +1,68 @@
+package com.example.sqlitetutorial;
+
+public class CustomerModel {
+
+    private int id;
+    private String name;
+    private int age;
+    private boolean isActive;
+
+    public CustomerModel(int id, String name, int age, boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.isActive = isActive;
+    }
+
+    // toString - for printing
+
+    @Override
+    public String toString() {
+        return "CustomerModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", isActive=" + isActive +
+                '}';
+    }
+
+
+    // Getters and setters
+
+    public CustomerModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+
+}
--- app/src/main/java/com/example/sqlitetutorial/MainActivity.java
+++ app/src/main/java/com/example/sqlitetutorial/MainActivity.java
@@ -2,13 +2,70 @@ package com.example.sqlitetutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // create references to buttons and other controls
+    Button btn_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activeCustomer;
+    ListView lv_customerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // define buttons/controls
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_age = findViewById(R.id.et_age);
+        et_name = findViewById(R.id.et_name);
+        sw_activeCustomer = findViewById(R.id.sw_active);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        // button listeners
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CustomerModel customerModel;
+
+                try {
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(),
+                            Integer.parseInt(et_age.getText().toString()), sw_activeCustomer.isChecked());
+
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e) {
+                    customerModel = new CustomerModel(-1, "error", 0, false);
+                    Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                }
+
+                DatabaseHelper databaseHelper = new DatabaseHelper(MainActivity.this);
+
+                boolean success = databaseHelper.addOne(customerModel);
+                Toast.makeText(MainActivity.this, "Success = " + success, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View All button", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
 }
